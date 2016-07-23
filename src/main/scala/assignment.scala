e613d51840100e1eb60dd05d93b80d2dc8658998
arpikulria <arpit.kulria@gmail.com>
2016-07-23 12:02:13 +0530

    Added question 6's solution

--- src/main/scala/assignment.scala
+++ src/main/scala/assignment.scala
@@ -5,15 +5,16 @@ import org.apache.spark.SparkConf
 object assignment1 extends App {
   val conf = new SparkConf().setAppName("appName").setMaster("local")
   val sc = new SparkContext(conf)
-  val pagecounts = sc.textFile("/home/arpit/Documents/Spark/pagecounts")
+  //https://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-12/pagecounts-20151201-220000.gz
+  val pagecounts = sc.textFile("/pagecounts/file/path or use above link ")
   val enCountRDD = pagecounts.flatMap(_.split(' ')).filter(_.contains("/en/"))
   val enCount = enCountRDD.collect.length
+  val q5Ans  = pagecounts.map (x => (x.split(' ')(1), x.split(' ')(2))).reduceByKey((a,b) => a+b).filter(_._2.toDouble > 200000)
+  println(s" n\n\n\n\n\n\n    ANS question 5.count  >>>>   ${q5Ans.count()} \n\n\n\n\n\n")
+  println(s" n\n\n\n\n\n\n    ANS question 5.take(100) >>>>   ${q5Ans.take(100).toList} \n\n\n\n\n\n")
   println(s"\n\n--page count RDD ---${pagecounts}--------")
   println(s"\n\n--en counts ---${enCount}--------")
   println(s"\n\n--en counts RDD---${enCountRDD}--------")
   println("\n\n Take 10 " + pagecounts.take(10).toList)
   println(s"\n\n --count---${pagecounts.count()}--------")
-
-  
-
 }
