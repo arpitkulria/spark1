af416b821ab5360ed8d2437b9e986bb46cefd19c
arpikulria <arpit.kulria@gmail.com>
2016-07-23 13:16:48 +0530

    Updated ans 4

--- src/main/scala/assignment.scala
+++ src/main/scala/assignment.scala
@@ -7,8 +7,8 @@ object assignment1 extends App {
   val sc = new SparkContext(conf)
   //https://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-12/pagecounts-20151201-220000.gz
   val pagecounts = sc.textFile("/pagecounts/file/path or use above link ")
-  val enCountRDD = pagecounts.flatMap(_.split(' ')).filter(_.contains("/en/"))
-  val enCount = enCountRDD.collect.length
+  val enCountRDD = pagecounts.map(_.split(' ')).filter(x => x(0) == "en")
+  val enCount = enCountRDD.count
   val q5Ans  = pagecounts.map (x => (x.split(' ')(1), x.split(' ')(2))).reduceByKey((a,b) => a+b).filter(_._2.toDouble > 200000)
   println(s" n\n\n\n\n\n\n    ANS question 5.count  >>>>   ${q5Ans.count()} \n\n\n\n\n\n")
   println(s" n\n\n\n\n\n\n    ANS question 5.take(100) >>>>   ${q5Ans.take(100).toList} \n\n\n\n\n\n")
